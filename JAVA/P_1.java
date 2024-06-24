519886a13261953b2d1fc312b180558b19671382
Jay Vegad <143251488+Vegadjay@users.noreply.github.com>
2024-06-24 16:16:29 +0530

    Add files via upload

--- JAVA/P_1.java
+++ JAVA/P_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class P1 {
+public class P_1 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.print("Enter the radius of the circle: ");
