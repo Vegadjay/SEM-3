301a09d1511dc577ee81ccb3efbd773e82b5cf69
Jay Vegad <143251488+Vegadjay@users.noreply.github.com>
2024-06-24 16:05:10 +0530

    Update P_15.java

--- JAVA/P_15.java
+++ JAVA/P_15.java
@@ -1,6 +1,6 @@
 // Avarage of first n numbers
 import java.util.Scanner;
-public class Lab_15 {
+public class P_15 {
     public static void main(String[] args) {
         int sum = 0;
         float avg = 0;
