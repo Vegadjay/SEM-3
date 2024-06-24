9008d0dc53cb5883ca869751131f8250236d5c7e
Jay Vegad <143251488+Vegadjay@users.noreply.github.com>
2024-06-24 16:16:47 +0530

    Add files via upload

--- JAVA/P_10.java
+++ JAVA/P_10.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class P10 {
+public class P_10 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the number of days");
