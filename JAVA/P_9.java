7b52379c0d1ed71aba010654ea4b045cf1a25540
Jay Vegad <143251488+Vegadjay@users.noreply.github.com>
2024-06-24 16:19:13 +0530

    Add files via upload

--- JAVA/P_9.java
+++ JAVA/P_9.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class P9 {
+public class P_9 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter seconds");
