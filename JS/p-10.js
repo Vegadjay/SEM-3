56e20c045fee1bfc482d84c33a5f446ec88a71ab
Jay Vegad <143251488+Vegadjay@users.noreply.github.com>
2024-06-25 09:18:32 +0530

    Add files via upload

---
+++ JS/p-10.js
@@ -0,0 +1,34 @@
+//  Demonstrate different array functions like push, pop, shift, unshift, splice, sort etc… (A)
+arr = ["Ramesh","Suresh","Kamlesh"];
+
+arr.push("Mahesh");
+
+console.log(arr);
+
+arr.pop();
+
+console.log(arr);
+
+arr.shift();
+
+console.log(arr);
+
+arr.unshift("Haresh");
+
+console.log(arr);
+
+arr.splice(2,1);
+
+console.log(arr);
+
+arr.push("Arvind");
+
+console.log("Before Sorting: ")
+
+console.log(arr);
+
+arr.sort();
+
+console.log("After Sorting: ");
+
+console.log(arr);
