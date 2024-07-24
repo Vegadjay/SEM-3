f047e9e3df2caa047a04a4817617b11f85f3a1c3
Jay Vegad <jayvegad10@gmail.com>
2024-07-24 16:13:06 +0530

    intial commit

--- JAVA/P_51.java
+++ JAVA/P_51.java
@@ -13,15 +13,87 @@ class Queue {
 
     Queue() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Size of the Queue: ");
+        System.out.print("Enter Size of the Q: ");
         this.size = sc.nextInt();
         this.top = -1;
         this.s = new int[this.size];
     }
+
+    int r = -1;
+    int f = -1;
+
+    public void Insert(int x) {
+        if (r + 1 >= this.size) {
+            System.out.println("Q is overflow");
+        } else if (f == -1) {
+            f++;
+            r++;
+            s[r] = x;
+        } else if (r + 1 >= size && f != 0) {
+            r = -1;
+        } else {
+            r++;
+            s[r] = x;
+        }
+    }
+
+    public void Delete() {
+        if (f == -1) {
+            System.out.println("Q is underflow");
+        } else if (f + 1 >= size) {
+            f = 0;
+        } else {
+            s[f] = 0;
+            f++;
+        }
+    }
+
+    public void display() {
+
+        System.out.print("[");
+        for (int i = f; i <= r; i++) {
+            if (f > r && f + 1 >= size) {
+                i = 0;
+            }
+            System.out.print(s[i] + "|");
+        }
+        System.out.println("]");
+    }
+
 }
 
 public class P_51 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue s = new Queue();
 
+        while (true) {
+            System.out.print(
+                    "Enter 1 for NQ, 2 for DQ, 3 for display Q, 4 for exit: ");
+            int userNum = sc.nextInt();
+
+            if (userNum == 4) {
+                break;
+            }
+
+            switch (userNum) {
+                case 1:
+                    System.out.print("Enter number to push in stack: ");
+                    int num = sc.nextInt();
+                    s.Insert(num);
+                    break;
+                case 2:
+                    s.Delete();
+                    break;
+                case 3:
+                    s.display();
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                    break;
+            }
+        }
+        System.out.println("Program ended.");
+        sc.close();
     }
 }
--- JAVA/P_65.java
+++ JAVA/P_65.java
@@ -1,84 +1,86 @@
-// Write a program to sort elements of a linked list.
 public class P_65 {
     class Node {
         int info;
         Node link;
-
+        
         public Node(int info) {
             this.info = info;
             this.link = null;
         }
     }
-
+    
     public Node first = null;
-
-    public void insertAtlast(int data) {
+    
+    public void insertAtLast(int data) {
         Node newNode = new Node(data);
-
+        
         if (first == null) {
             first = newNode;
             return;
         }
-
+        
         Node last = first;
         while (last.link != null) {
             last = last.link;
         }
         last.link = newNode;
     }
-
-    public void insertAtfirst(int data) {
+    
+    public void insertAtFirst(int data) {
         Node newNode = new Node(data);
         if (first == null) {
             first = newNode;
             return;
         }
-
+        
         Node temp = first;
         first = newNode;
         newNode.link = temp;
     }
-
+    
     public void sortLink() {
-        if (first == null) {
-            System.out.println("List is empty");
-            return;
+        if (first == null || first.link == null) {
+            return; // List is empty or has only one element, no need to sort
         }
-        boolean isTrue = true;
-        while (isTrue) {
-            Node temp1 = first;
-            Node temp2 = first.link;
-            Node save = null;
-            while (temp2 != null) {
-                if (temp1.info > temp2.info) {
-                    if (save != null) {
-                        Node temp = temp2.link;
-                        save.link = temp2;
-                        temp2.link = temp1;
-                        temp1.link = temp;
+
+        boolean swapped;
+        do {
+            swapped = false;
+            Node prev = null;
+            Node current = first;
+            Node next = first.link;
+
+            while (next != null) {
+                if (current.info > next.info) {
+                    swapped = true;
+
+                    if (prev == null) {
+                        // Swapping at the beginning of the list
+                        first = next;
                     } else {
-                        Node temp = temp2.link;
-                        first = temp2;
-                        temp2.link = temp1;
-                        temp1.link = temp;
+                        prev.link = next;
                     }
-                    save = temp2;
-                    temp2 = temp1.link;
+
+                    current.link = next.link;
+                    next.link = current;
+
+                    prev = next;
+                    next = current.link;
                 } else {
-                    save = temp1;
-                    temp1 = temp2;
-                    temp2 = temp2.link;
+                    prev = current;
+                    current = next;
+                    next = next.link;
                 }
             }
-        }
+        } while (swapped);
     }
-
+    
     public void print() {
         if (first == null) {
             System.out.println("List is empty");
             return;
         }
-
+        
         Node temp = first;
         while (temp != null) {
             System.out.print(temp.info + "->");
@@ -86,14 +88,17 @@ public class P_65 {
         }
         System.out.println("NULL");
     }
-
+    
     public static void main(String[] args) {
         P_65 list = new P_65();
-        list.insertAtfirst(30);
-        list.insertAtfirst(20);
-        list.insertAtfirst(10);
-        list.insertAtfirst(50);
+        list.insertAtFirst(30);
+        list.insertAtFirst(20);
+        list.insertAtFirst(10);
+        list.insertAtFirst(50);
+        System.out.println("Original list:");
+        list.print();
         list.sortLink();
+        System.out.println("Sorted list:");
         list.print();
     }
 }
