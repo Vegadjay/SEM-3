71d881eef0bf5dae526853fb01fcf40ef1ffac51
Jay Vegad <jayvegad10@gmail.com>
2024-07-25 22:47:20 +0530

    Adding OS Module Methods

--- JS/osmodule/index.js
+++ JS/osmodule/index.js
@@ -1,12 +1,13 @@
 const os = require("os");
 
-// console.log(os.arch()); // x64
-// console.log(os.cpus()); // give the data of our cpu and other
-// console.log(os.endianness()); // return le and be
-// console.log(os.freemem()); // direct this is shown in byte
-// console.log(os.freemem() / 1024 / 1024 / 1024, "GB"); // this is shown as gb
-// console.log(os.networkInterfaces()); // this is showing the wifi configration
-// console.log(os.release());
-// e.g., '10.0.19042'
-// console.log(os.type());
-// e.g., 'Linux', 'Darwin', 'Windows_NT'
+console.log(os.arch()); // x64
+console.log(os.cpus()); // give the data of our cpu and other
+console.log(os.endianness()); // return le and be
+console.log(os.freemem()); // direct this is shown in byte
+console.log(os.freemem() / 1024 / 1024 / 1024, "GB"); // this is shown as gb
+console.log(os.networkInterfaces()); // this is showing the wifi configration
+console.log(os.release());
+e.g., '10.0.19042'
+console.log(os.type());
+e.g., 'Linux', 'Darwin', 'Windows_NT'
+console.log(os.machine()) //x86_64
