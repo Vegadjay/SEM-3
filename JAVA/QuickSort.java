75b87046588a6dc609a9e384fb44a2582c237bcd
Jay Vegad <jayvegad10@gmail.com>
2024-09-04 07:32:12 +0530

    Quick Sort Added

---
+++ JAVA/QuickSort.java
@@ -0,0 +1,46 @@
+public class QuickSort {
+    public static void Quick_Sort(int arr[],int LB, int UB) {
+        boolean flag = true;
+        if(LB < UB) {
+            int i = LB;
+            int j = UB + 1;
+            int key = arr[LB];
+            while(flag) {
+                i = i + 1;
+                while (i <= UB && arr[i] < key) {
+                    i = i + 1;
+                }
+                j = j - 1;
+                while (j >= LB && arr[j] > key) {
+                    j = j - 1;
+                }
+                if(i<j) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                else {
+                    flag = false;
+                }
+                int temp = arr[LB];
+                arr[LB] = arr[j];
+                arr[j] = temp;
+                Quick_Sort(arr,LB,j-1);
+                Quick_Sort(arr, j+1, UB);
+            }
+        }
+    }
+
+    public static void print(int arr[]) {
+        for(int i = 0; i<arr.length; i++) {
+            System.out.print(arr[i] + ",");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+        int n = arr.length;
+        Quick_Sort(arr, 0, n-1);
+        print(arr);
+    }
+}
+
