082ac431b2547aa1008b6f916d1b5166bc339547
Jay Vegad <jayvegad10@gmail.com>
2024-09-04 07:53:52 +0530

    Quick Sort Added

--- JAVA/QuickSort.java
+++ JAVA/QuickSort.java
@@ -22,12 +22,12 @@ public class QuickSort {
                 else {
                     flag = false;
                 }
-                int temp = arr[LB];
-                arr[LB] = arr[j];
-                arr[j] = temp;
-                Quick_Sort(arr,LB,j-1);
-                Quick_Sort(arr, j+1, UB);
             }
+            int temp = arr[LB];
+            arr[LB] = arr[j];
+            arr[j] = temp;
+            Quick_Sort(arr,LB,j-1);
+            Quick_Sort(arr, j+1, UB);
         }
     }
 
@@ -37,7 +37,7 @@ public class QuickSort {
         }
     }
     public static void main(String[] args) {
-        int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+        int arr[] = { 22,23,34,54,6543,21,1,22,1,1,1,1,345,6,3,454 };
         int n = arr.length;
         Quick_Sort(arr, 0, n-1);
         print(arr);
