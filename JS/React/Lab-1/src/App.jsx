85b5899862d5d2b9e13c8537d4d570f7a9c4fbec
Jay Vegad <jayvegad10@gmail.com>
2024-09-10 03:54:49 +0530

    intial commit

---
+++ JS/React/Lab-1/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './componants/Navbar'
+
+function App() {
+
+  return (
+    <div className='h-100 w-100 bg-black'>
+    <div>
+      <Navbar/>    
+     </div>
+    </div>
+  )
+}
+
+export default App
